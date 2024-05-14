93487aace5cad763b346da3ce6d006a635b6d487
nirgodin <70273595+nirgodin@users.noreply.github.com>
2024-05-14 19:29:51 +0300

    Changed streamer to work with bytes instead of file

--- genie_radio/radio_streamer.py
+++ genie_radio/radio_streamer.py
@@ -1,5 +1,5 @@
-import os.path
-import time
+from io import BytesIO
+from time import time
 
 from aiohttp import ClientSession, ClientResponse
 from genie_common.tools import logger
@@ -9,25 +9,23 @@ class RadioStreamer:
     def __init__(self, session: ClientSession):
         self._session = session
 
-    async def stream(self, dir_path: str, url: str) -> str:
-        file_path = os.path.join(dir_path, "radio.ogg")
-
+    async def stream(self, url: str) -> bytes:
         async with self._session.get(url) as response:
-            with open(file_path, "wb") as file:
-                await self._write(response, file)
+            stream = BytesIO()
+            await self._write(response, stream)
 
-        return file_path
+        return stream.getvalue()
 
     @staticmethod
-    async def _write(response: ClientResponse, file):
-        start_time = time.time()
+    async def _write(response: ClientResponse, stream: BytesIO) -> None:
+        logger.info("Streaming track for the next 8 seconds")
+        start_time = time()
 
-        while time.time() - start_time < 8:
-            logger.info("Streaming")
+        while time() - start_time < 8:
             chunk = await response.content.read(1024)
 
             if not chunk:
                 break
 
-            file.write(chunk)
-            file.flush()
+            stream.write(chunk)
+            stream.flush()
